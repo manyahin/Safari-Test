8810cc7f5b74e95eb7a4dd8500d25bca449eed21
Ilya Manyahin <i.manyahin@gmail.com>
2020-08-28 17:05:28 +0300

    format, finish

--- README.md
+++ README.md
@@ -1,6 +1,35 @@
-# Safari Field Task
+# Safari Field Test
 
-todo:
-* formatting
-* animation ? :)
-* check big field perfomance
+    python app.py
+
+## Example Result
+
+```bash
+Board at start:
+
+D D W T W D T L L D
+D L T L D L D T W L
+D D T D T W D D T W
+W T L W W T L T W D
+D W T L W T L W W W
+T T T T W D D L W T
+L L W L L W T T W D
+L L W L D T D L T D
+D T T W T D T W W D
+D W D T L T W L L W
+
+Chosen cell: [4, 6]: T
+
+Board after conquer:
+
+D D W T - - T L L -
+D L T L - L - T - L
+D D T - T - - - T -
+W T L - - - L T - -
+D W T L - T L - - -
+T T T T - - - L - T
+L L - L L - T T - -
+L L - L - T - L T -
+- T T - T - T - - -
+- - - T L T - L L -
+```
--- app.py
+++ app.py
@@ -1,33 +1,43 @@
-# required Python 3.8.2
+#!/usr/bin/python
 
 from abc import ABC, abstractmethod
 import random
+import copy
+import sys
 
-class SafariField:
+if sys.version_info[0] < 3 or sys.version_info[1] < 6:
+  print('Required Python version 3.6 or higher')
+  sys.exit()
+
+# Python has recursion limit, so big field will not work with that algorithm
+# print('Recursion limit:', sys.getrecursionlimit())
+
+
+class Safari:
   def __init__(self, height=10, width=10):
     self.size = {"height": height, "width": width}
-    self.field = []
+    self.matrix = []
     self.animals = ('Lion', 'Tiger', 'Wolf', 'Deer')
 
     self.generate()
+    print('Board at start:')
+    self.render()
 
   def generate(self):
     for h in range(self.size['height']):
-      self.field.append([])
+      self.matrix.append([])
       for w in range(self.size['width']):
         animal_type = self.get_random_animal_type()
         animal = self.make_animal(animal_type)
-        animal.position = {'x': w, 'y': h}
+        animal.position = {'y': h, 'x': w}
 
-        self.field[h].append(animal)
+        self.matrix[h].append(animal)
 
   def render(self):
-    print('Safari Field - a danger place')
-    # todo: print field at start
-    for h in range(len(self.field)):
-      print() # new line
-      for w in range(len(self.field)):
-        print(self.field[h][w].symbol, end = ' ')
+    for h in range(len(self.matrix)):
+      print()  # new line
+      for w in range(len(self.matrix[h])):
+        print(self.matrix[h][w].symbol, end=' ')
 
   def get_random_animal_type(self):
     return random.choice(self.animals)
@@ -42,44 +52,43 @@ class SafariField:
     elif animal_type == 'Deer':
       return Deer(self)
 
-    # todo: tried to make class dinamicly using polymorphism, didn't success
-    # random_animal = type(random_animal_name, (random_animal_name,), {
-    #   "__metaclass__": Animal
-    # })
-  
   def get_random_cell(self):
-    x = random.randrange(self.size['width'])
     y = random.randrange(self.size['height'])
-    return self.field[x][y]
+    x = random.randrange(self.size['width'])
+    print(f'\n\nChosen cell: [{y + 1}, {x + 1}]: {self.matrix[y][x].symbol}')
+    return self.matrix[y][x]
 
-  # todo: start_fight, theriocide 
-  def start_conquer(self):
-    print('start')
+  def live(self):
     blessed_animal = self.get_random_cell()
     blessed_animal.conquer()
     pass
 
   def get_neighbors(self, animal):
     neighbors = []
-    # todo: change to objects to easy understand
     relatives = [
-      # y,  x
-      [-1,  0], # up
-      [-1,  0], # up
-      [-1,  1], # upRight
-      [ 0,  1], # right
-      [ 1,  1], # downRight
-      [ 1,  0], # down
-      [ 1, -1], # downLeft
-      [ 0, -1], # left
-      [-1, -1], # upLeft
+        {'y': -1, 'x':  0},  # up
+        {'y': -1, 'x':  1},  # upRight
+        {'y':  0, 'x':  1},  # right
+        {'y':  1, 'x':  1},  # downRight
+        {'y':  1, 'x':  0},  # down
+        {'y':  1, 'x': -1},  # downLeft
+        {'y':  0, 'x': -1},  # left
+        {'y': -1, 'x': -1},  # upLeft
     ]
 
     for r in relatives:
       try:
-        neighbor = self.field[animal.position["y"] - r[0]][animal.position["x"] - r[1]]
+        y = animal.position['y'] - r['y']
+        x = animal.position['x'] - r['x']
+
+        # prevent negative indexes because Python allow it by default
+        if x < 0 or y < 0:
+          raise IndexError('Negative indexes not allowed')
+
+        neighbor = self.matrix[y][x]
         neighbors.append(neighbor)
       except IndexError:
+        # out of border, do nothing
         pass
 
     return neighbors
@@ -89,7 +98,13 @@ class SafariField:
 class Animal(ABC):
   def __init__(self, field):
     self.field = field
-    self.position = {"x": 0, "y": 0}
+    self.position = {"y": 0, "x": 0}
+    '''
+        the counter with an amount of required iterations to do
+        use Python language property that reference to object is the same for cloned objects
+        thus this counter will be the same for all copied objects of the same animal
+        '''
+    self.steps_todo = {'count': 1}
 
   @property
   @abstractmethod
@@ -106,59 +121,69 @@ class Animal(ABC):
     pass
 
   def conquer(self):
-    # get fields around
     '''
-    1. attach field to each animal
-       get cells around
-       run conqure on each of them
-    2. fill animals with around fields
-    3. make static method for fields to find them
+    1. move the animal out of the cell
+    2. get neighboars and check which of them can be beated
+    3. copy the animal to possible cells
+    4. execute the same logic for other cells with the animal (recursion)
     '''
-    print('I am', self.type)
-
-    # move itself out
-    # todo: should I move if no beat nobody?
-    self.field.field[self.position['y']][self.position['x']] = Nobody(self.field)
+    # move the animal out of the cell
+    self.field.matrix[self.position['y']][self.position['x']] = Nobody(self.field)
+    self.steps_todo['count'] -= 1
 
     neighbors = self.field.get_neighbors(self)
+    animal_copies = []
 
     for n in neighbors:
       if n.type in self.beat:
-        print('i win', n.type)
-        # move animal to next cell
-        self.field.field[n.position['y']][n.position['x']] = self
-        self.field.field[n.position['y']][n.position['x']].conquer()
+        self.steps_todo['count'] += 1
 
-    # todo: if am I deer?
-    if not neighbors:
+        # copy the predator to next cells
+        animal_copy = copy.copy(self)
+        animal_copy.position = {'x': n.position['x'], 'y': n.position['y']}
+        animal_copies.append(animal_copy)
+
+        # update field with new position of animals
+        self.field.matrix[n.position['y']][n.position['x']] = animal_copy
+
+    # check if there is more available cells to discover
+    if not self.steps_todo['count']:
+      print('\nBoard after conquer:')
       self.field.render()
+    else:
+      for a in animal_copies:
+        a.conquer()
 
 
 class Lion(Animal):
   type = 'Lion'
   beat = ('Tiger', 'Wolf', 'Deer')
-    
+
+
 class Tiger(Animal):
   type = 'Tiger'
   beat = ('Wolf', 'Deer')
+
 
 class Wolf(Animal):
   type = 'Wolf'
   beat = ('Deer')
 
+
 class Deer(Animal):
   type = 'Deer'
-  beat = () # poor deer
+  beat = ()  # poor deer
+
 
 class Nobody(Animal):
+  # dummy animal to fill the board with dead bodies
   type = '-'
   beat = ()
 
 
 def main():
-  field = SafariField()
-  field.render()
-  field.start_conquer()
+  safari = Safari()
+  safari.live()
 
 
 if __name__ == "__main__":
